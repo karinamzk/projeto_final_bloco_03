8ffa68f091299fec110b2a0e2a7ff8ca4442c983
Karina Akina <karinamzk2@gmail.com>
2023-11-09 10:17:13 -0300

    Primeira Etapa - Construção do Projeto e Estilização

---
+++ farmacia/src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+            
+                <div className="container flex justify-between text-lg">
+                    Farmacia - Sempre ao seu lado
+                    <div className='flex gap-4'>
+                        Produtos
+                        Categorias
+                        Cadastrar Categorias 
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ farmacia/src/pages/home/Home.tsx
@@ -0,0 +1,37 @@
+function Home() {
+    return (
+        <>
+            <div style={{
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <div>
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <h1>Farmacia - sempre ao seu lado!</h1>
+                    </div>
+
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <img 
+                            src="https://img.freepik.com/vetores-premium/remedio-farmacia-logotipo-de-saude-medico-herbal-plus-icone-simbolo-de-saude-desenho-vetorial_10135-1110.jpg?w=826" 
+                            alt="Imagem da Página Home" 
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
