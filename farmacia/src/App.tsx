d838ac783634c83294edcd02723d67dae07cdab5
Karina Akina <karinamzk2@gmail.com>
2023-11-09 10:52:01 -0300

    Primeira Etapa - Construção do Projeto e Estilização Update

--- farmacia/src/App.tsx
+++ farmacia/src/App.tsx
@@ -2,14 +2,22 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Footer from './components/footer/Footer'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
-    return (
-      <>
-      <Navbar />
-      <Home />
-      <Footer />
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-    )
+  );
 }
 
 export default App
--- farmacia/src/pages/home/Home.tsx
+++ farmacia/src/pages/home/Home.tsx
@@ -1,35 +1,30 @@
 function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
-                <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <h1>Farmacia - sempre ao seu lado!</h1>
+            <div className="bg-[#568ce3] flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vinde!
+                        </h2>
+                        <p className='text-xl'>
+                            Farmacia - Sempre ao seu lado!
+                        </p>
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                                >
+                                Nova Postagem
+                            </div>
+                        </div>
                     </div>
-
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
                         <img 
                             src="https://img.freepik.com/vetores-premium/remedio-farmacia-logotipo-de-saude-medico-herbal-plus-icone-simbolo-de-saude-desenho-vetorial_10135-1110.jpg?w=826" 
                             alt="Imagem da Página Home" 
-                            width="400px"
+                            className='w-2/3'
                         />
                     </div>
                 </div>
-            </div>
         </>
     )
 }
